740f0698d2fb0da5929d94bf714a841ca0ba6548
Moondip Konwar <moondipkonwar9@gmail.com>
2026-01-29 17:34:40 +0530

    Google Material Symbols + Google Sans font + Navbar

---
+++ src/lib/components/Navbar.svelte
@@ -0,0 +1,45 @@
+<nav class="fixed h-15 w-full bg-surface flex flex-row items-center">
+  <!-- Menu Button  -->
+  <span class="nav-item material-symbols-outlined text-primary-text">
+    menu
+  </span>
+
+  <!-- Title  -->
+  <div class="nav-item text-primary flex flex-row items-center gap-2">
+    <span class="material-symbols-outlined"> circle_circle </span>
+    <span class="text-xl font-black">Anishelf</span>
+  </div>
+
+  <!-- Nav Buttons: Desktop -->
+  <div class="flex flex-row items-center text-secondary-text">
+    <button class="nav-button">New</button>
+    <button class="nav-button">Popular</button>
+    <button class="nav-button">Discover</button>
+    <button class="nav-button">Continue</button>
+  </div>
+
+  <!-- Middle empty space -->
+  <div class="w-full"></div>
+
+  <!-- Actions -->
+  <div class="flex flex-row items-center text-secondary-text">
+    <span class="material-symbols-outlined nav-button">search</span>
+    <span class="material-symbols-outlined nav-button">account_circle</span>
+  </div>
+</nav>
+
+<style>
+  .nav-item {
+    margin: 0 1rem;
+  }
+
+  .nav-button {
+    height: 100%;
+    padding: 2rem 1rem;
+  }
+
+  .nav-button:hover {
+    background-color: var(--color-background);
+    color: white;
+  }
+</style>
